695bd31e9a09408145971cb4af6cd1d2dd26cb14
wangkyo <jwksimple@gmail.com>
2020-12-13 20:24:58 +0900

    update

---
+++ js/firestore.js
@@ -0,0 +1,55 @@
+let db;
+
+function dbInitialize() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBUZil_3m-xEDFoqB499LOhSZjQF7qhsEk",
+      authDomain: "wedding-invitation-5e95e.firebaseapp.com",
+      projectId: "wedding-invitation-5e95e"
+    });
+
+    db = firebase.firestore();
+}
+
+function dbReadAllMessages() {
+    db.collection("message").get().then((querySnapshot) => {
+        console.log(querySnapshot.docs.map(doc => doc.data()));
+        setMessages(querySnapshot.docs.map(doc => doc.data()));
+    });
+}
+
+function dbCreateMessage(input_name, input_comment, input_password) {
+    db.collection("message").add({
+        name: input_name,
+        comment: input_comment,
+        password: input_password,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        updateMessages();
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
+
+//function dbReadMessage(message_id) {
+//    db.collection("message").doc(message_id).get().then(function(doc) {
+//        if (doc.exists) {
+//            console.log("Document data:", doc.data());
+//        } else {
+//            console.log("No such document!");
+//        }
+//    }).catch(function(error) {
+//        console.log("Error getting document:", error);
+//    });
+//}
+
+function dbDeleteMessage(message_id) {
+    db.collection("message").doc(message_id).delete().then(function() {
+        console.log("Document successfully deleted!");
+        updateMessages();
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
 let PHOTOS = "";
-let VISIBLE_COUNT = "";
-let TOTAL_COUNT = "";
+let VISIBLE_PHOTO_COUNT = "";
+let TOTAL_PHOTO_COUNT = "";
+
+let MESSAGES = "";
+let VISIBLE_MESSAGE_COUNT = "";
+let TOTAL_MESSAGE_COUNT = "";
+
+$(document).ready(function() {
+    try {
+        dbInitialize();
+        updateMessages();
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 function setPhotos(json) {
     PHOTOS = json["photo_list"];
-    VISIBLE_COUNT = json["visible_photo_count"];
-    TOTAL_COUNT = Object.keys(PHOTOS).length;
+    VISIBLE_PHOTO_COUNT = json["visible_photo_count"];
+    TOTAL_PHOTO_COUNT = Object.keys(PHOTOS).length;
 
     $.each(PHOTOS, function(index, photo) {
         $("#thumbnail_list").append(
@@ -16,10 +29,8 @@ function setPhotos(json) {
             "</div>"
         );
 
-        if(index > VISIBLE_COUNT) $("#"+index).hide();
-    });
+        if(index > VISIBLE_PHOTO_COUNT) $("#"+index).hide();
 
-    $.each(PHOTOS, function(index, photo) {
         if(index == 1) {
             $("#carousel_list").append(
                 "<div class='carousel-item active'>" +
@@ -41,7 +52,7 @@ $.getJSON("resource/photo.json", function(json) {
 });
 
 function showMorePhotos() {
-    for(index=VISIBLE_COUNT+1; index<=TOTAL_COUNT; index++) {
+    for(index=VISIBLE_PHOTO_COUNT+1; index<=TOTAL_PHOTO_COUNT; index++) {
         $("#"+index).show();
     }
     $("#more_button").hide();
@@ -65,4 +76,98 @@ function disableScroll() {
 
 function enableScroll() {
     $("body").css("overflow", "visible");
+}
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                event.preventDefault();
+                $('#add_message').modal('hide')
+
+                const name = $("#name").val();
+                const comment = $("#comment").val();
+                const password = $("#password").val();
+
+                try {
+                    dbCreateMessage(name, comment, password);
+                } catch (error) {
+                  console.error(error);
+                }
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+  }, false);
+})();
+
+function initializeMessageForm() {
+    var forms = document.getElementsByClassName('needs-validation');
+    $.each(forms, function(index, form) {
+        form.reset();
+        form.classList.remove('was-validated');
+    });
+}
+
+function validateNumber() {
+    if((event.keyCode<48)||(event.keyCode>57)) {
+        event.returnValue=false;
+    }
+}
+
+function updateMessages() {
+    try {
+        dbReadAllMessages();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function setMessages(messages) {
+    MESSAGES = messages;
+    TOTAL_MESSAGE_COUNT = Object.keys(MESSAGES).length;
+    VISIBLE_MESSAGE_COUNT = 5;
+
+    $("#message_list").empty();
+
+    $.each(MESSAGES, function(index, message) {
+        $("#message_list").append(
+            "<li id='m"+index+"' class='list-group-item'>" +
+                "<div class='card-body message-card'>" +
+                    "<div class='row'>" +
+                        "<div class='col'>" +
+                            "<h5 class='card-title message-name'>" + message["name"] + "</h5>" +
+                        "</div>" +
+                        "<div class='col'>" +
+                            "<button type='button' class='close message-delete-button'>" +
+                                "<span aria-hidden='true'>&times;</span>" +
+                            "</button>" +
+                        "</div>" +
+                    "</div>" +
+                    "<p class='card-text message-comment'>" + message["comment"] + "</p>" +
+                    "<p class='card-text message-time'><small class='text-muted'>" + message["timestamp"].toDate("YYYY-MM-DD") + "</small></p>" +
+                    "<input type='hidden' id='p"+index+"' value='" + message["password"] + "'>" +
+                "</div>" +
+            "</li>"
+        );
+
+        if(index > VISIBLE_MESSAGE_COUNT) $("#m"+index).hide();
+    });
+}
+
+function showMoreMessages() {
+    for(index=VISIBLE_MESSAGE_COUNT+1; index<=TOTAL_MESSAGE_COUNT; index++) {
+        $("#m"+index).show();
+    }
+    $("#more_button").hide();
+}
+
+function deleteMessage() {
+
 }
